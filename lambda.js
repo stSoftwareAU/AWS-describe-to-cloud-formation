0224239d508546d8fe12ab94ded9cdbf69c1b452
Nigel Leck <monster.flying.s@gmail.com>
2019-05-12 20:24:17 +1000

    Update lambda.js

--- lambda.js
+++ lambda.js
@@ -1,6 +1,6 @@
 exports.handler = async(event) => {
 
-    console.info(event);
+//     console.info(event);
 
     let resourses = {};
 
@@ -17,7 +17,6 @@ exports.handler = async(event) => {
         for (let i = 0; i < len; i++) {
             launchTemplateVersions(resourses, ltv[i]);
         }
-
     }
 
     let responseCode = 200;
@@ -28,7 +27,7 @@ exports.handler = async(event) => {
         //        body: JSON.stringify(responseBody)
         body: responseBody
     };
-    console.log("response: " + JSON.stringify(response))
+//     console.log("response: " + JSON.stringify(response))
     return response;
 };
 
@@ -43,31 +42,30 @@ function safeName(name) {
 
 function b64Value(b64) {
 
-
     let buff = new Buffer(b64, 'base64');
     let text = buff.toString('ascii');
 
     let joinObj = {
         "Fn::Join": ["\n", text.split('\n')]
-        };
+    };
 
-        let obj = { "Fn::Base64": joinObj };
+    let obj = { "Fn::Base64": joinObj };
 
-        return obj;
+    return obj;
+}
+
+function launchTemplateVersions(resources, ltv) {
+
+    if (ltv.DefaultVersion) {
+
+        let item = {};
+        item.Type = "AWS::EC2::LaunchTemplate";
+        item.Properties = {};
+        item.Properties.LaunchTemplateName = ltv.LaunchTemplateName;
+
+        item.Properties.LaunchTemplateData = ltv.LaunchTemplateData;
+        item.Properties.LaunchTemplateData.UserData = b64Value(ltv.LaunchTemplateData.UserData);
+
+        resources['LaunchTemplate' + safeName(ltv.LaunchTemplateName)] = item;
     }
-
-    function launchTemplateVersions(resources, ltv) {
-
-        if (ltv.DefaultVersion) {
-
-            let item = {};
-            item.Type = "AWS::EC2::LaunchTemplate";
-            item.Properties = {};
-            item.Properties.LaunchTemplateName = ltv.LaunchTemplateName;
-
-            item.Properties.LaunchTemplateData = ltv.LaunchTemplateData;
-            item.Properties.LaunchTemplateData.UserData = b64Value(ltv.LaunchTemplateData.UserData);
-
-            resources['LaunchTemplate' + safeName(ltv.LaunchTemplateName)] = item;
-        }
-    }
+}
